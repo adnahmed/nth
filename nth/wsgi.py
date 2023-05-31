6730ec432171fb2986912016c27cb857f24d283c
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-05-31 11:55:09 +0500

    default wsgi django config

--- nth/wsgi.py
+++ nth/wsgi.py
@@ -6,8 +6,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nth.config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
-
 from configurations.wsgi import get_wsgi_application  # noqa
+
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nth.settings')
+
 application = get_wsgi_application()
