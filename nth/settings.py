9324fabc7adbd78496f098da02872c486cc34558
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-05-31 22:18:48 +0500

    comment out all admin and auth from settings

--- nth/settings.py
+++ nth/settings.py
@@ -12,19 +12,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Common(Configuration):
     INSTALLED_APPS = (
-        "django.contrib.admin",
-        "django.contrib.auth",
+        # "django.contrib.admin",
+        # "django.contrib.auth",
         "django.contrib.contenttypes",
         "django.contrib.sessions",
         "django.contrib.messages",
         "django.contrib.staticfiles",
         # Third party apps
         "rest_framework",  # utilities for rest apis
-        "rest_framework.authtoken",  # token authentication
+        # "rest_framework.authtoken",  # token authentication
         "rest_framework_filters",  # for filtering rest endpoints
         # Your apps
-        "nth.tasks",
-        "snippets",
+        # "nth.tasks",
     )
 
     # https://docs.djangoproject.com/en/2.0/topics/http/middleware/
@@ -32,8 +31,8 @@ class Common(Configuration):
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
         "django.middleware.common.CommonMiddleware",
-        "django.middleware.csrf.CsrfViewMiddleware",
-        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        # "django.middleware.csrf.CsrfViewMiddleware",
+        # "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
     )
@@ -90,7 +89,7 @@ class Common(Configuration):
                 "context_processors": [
                     "django.template.context_processors.debug",
                     "django.template.context_processors.request",
-                    "django.contrib.auth.context_processors.auth",
+                    # "django.contrib.auth.context_processors.auth",
                     "django.contrib.messages.context_processors.messages",
                 ],
             },
@@ -101,22 +100,22 @@ class Common(Configuration):
     # https://docs.djangoproject.com/en/dev/ref/settings/#debug
     DEBUG = strtobool(os.getenv("DJANGO_DEBUG", "no"))
 
-    # Password Validation
-    # https://docs.djangoproject.com/en/2.0/topics/auth/passwords/#module-django.contrib.auth.password_validation
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-        },
-        {
-            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-        },
-        {
-            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-        },
-        {
-            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-        },
-    ]
+    # # Password Validation
+    # # https://docs.djangoproject.com/en/2.0/topics/auth/passwords/#module-django.contrib.auth.password_validation
+    # AUTH_PASSWORD_VALIDATORS = [
+    #     {
+    #         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    #     },
+    #     {
+    #         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    #     },
+    #     {
+    #         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    #     },
+    #     {
+    #         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    #     },
+    # ]
 
     # Logging
     LOGGING = {
@@ -178,19 +177,20 @@ class Common(Configuration):
     # Django Rest Framework
     REST_FRAMEWORK = {
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "UNAUTHENTICATED_USER": None,
         "PAGE_SIZE": int(os.getenv("DJANGO_PAGINATION_LIMIT", 10)),
         "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%S%z",
         "DEFAULT_RENDERER_CLASSES": (
             "rest_framework.renderers.JSONRenderer",
             "rest_framework.renderers.BrowsableAPIRenderer",
-        ),
-        "DEFAULT_PERMISSION_CLASSES": [
-            "rest_framework.permissions.IsAuthenticated",
-        ],
-        "DEFAULT_AUTHENTICATION_CLASSES": (
-            "rest_framework.authentication.SessionAuthentication",
-            "rest_framework.authentication.TokenAuthentication",
-        ),
+        )
+        # "DEFAULT_PERMISSION_CLASSES": [
+        #     "rest_framework.permissions.IsAuthenticated",
+        # ],
+        # "DEFAULT_AUTHENTICATION_CLASSES": (
+        #     "rest_framework.authentication.SessionAuthentication",
+        #     "rest_framework.authentication.TokenAuthentication",
+        # ),
     }
 
 
