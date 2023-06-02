b02f7d3486d14230cfdff495a7f7d176f2c6c9ab
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2023-06-03 01:03:24 +0500

    add task model

--- nth/que/models.py
+++ nth/que/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    # TODO: Add more fields
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.id
